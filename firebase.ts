0cd9a028cbbe7285279308f57440bef71eb76492
Davod <davodnikoyii@gmail.com>
2024-10-17 11:25:22 +0200

    Added envirenment variable for firebase key

--- firebase.ts
+++ firebase.ts
@@ -2,19 +2,18 @@ import { getAnalytics } from "firebase/analytics";
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-import { firebaseAPIKey } from "./env";
 
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: firebaseAPIKey,
-    authDomain: "bank-app-b6a7e.firebaseapp.com",
-    projectId: "bank-app-b6a7e",
-    storageBucket: "bank-app-b6a7e.appspot.com",
-    messagingSenderId: "818458394377",
-    appId: "1:818458394377:web:cb742f8b0cbaf30da21dd8",
-    measurementId: "G-J7RCPH4077"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: "bank-app-b6a7e.firebaseapp.com",
+  projectId: "bank-app-b6a7e",
+  storageBucket: "bank-app-b6a7e.appspot.com",
+  messagingSenderId: "818458394377",
+  appId: "1:818458394377:web:cb742f8b0cbaf30da21dd8",
+  measurementId: "G-J7RCPH4077",
 };
 
 // Initialize Firebase
@@ -24,5 +23,3 @@ const analytics = getAnalytics(app);
 // Initialize Firebase Authentication and Firestore and get a reference to the service
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-
-
